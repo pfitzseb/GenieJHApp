cfe3435b6daa7af3628656786efccd639fc2d06a
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-04-19 14:19:39 +0200

    init

---
+++ bin/main.jl
@@ -0,0 +1,21 @@
+# Genie Hello World!
+# As simple as Hello
+using Genie
+route("/hello") do
+    "Welcome to Genie!"
+end
+
+# Powerful high-performance HTML view templates
+using Genie.Renderer.Html
+route("/html") do
+    h1("Welcome to Genie!") |> html
+end
+
+# JSON rendering built in
+using Genie.Renderer.Json
+route("/json") do
+    (:greeting => "Welcome to Genie!") |> json
+end
+
+# Start the app!
+up(8888) 
---
+++ src/GenieJHApp.jl
@@ -0,0 +1,1 @@
+module GenieJHApp end
